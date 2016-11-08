38d069824ac2961ac68e05088e76a0738336d8f9
Nolan Skochdopole <naskoch@stanford.edu>
2016-11-08 10:45:44 -0800

    adding python file

---
+++ look_ahead_threshold.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def rev_rate_thresh(k, lam, beta, R, delta, t0, p):
+	# compute revenue rate with a threshold function
+	# with prob p, look-ahead = t1 >= delta
+	# with prob 1-p, look-ahead = t0 < delta
+	# reward R after k purchaces
+	# discouting factor beta and exogenous prob lam
+
+	return (lam*p*(k-R))/(k-(1.-lam)*delta)+(lam*(1.-p)*(k-R))/(k-(1.-lam)*t0)
+
+def compute_delta(v, R, beta):
+	# compute delta (related to phase transition) for 
+	# duopoly where reward is R, discouting is beta and 
+	# price difference is v
+
+	return np.floor(np.log(v/(R*beta*(1.-beta)))/np.log(beta))
+
+def solve_thresh(lam, R, delta, t0, p):
+	# solve for critical points of rev_rate_thresh function
+
+	val1 = R-(1.-lam)*delta
+	val2 = R-(1.-lam)*t0
+
+	a = p*val1+(1.-p)*val2
+	b = -2*(1.-lam)*(p*val1*t0+(1.-p)*val2*delta)
+	c = ((1.-lam)**2)*(p*val1*(t0**2)+(1.-p)*val2*(delta**2))
+
+	return np.roots([a, b, c])
+
+v = 0.1
+lam = 0.05
+beta = 0.9
+R = v*20.
+
+delta = compute_delta(v, R, beta)
+
+p = np.random.rand()
+t0 = 0
+
+#k = range(1,3*int(delta)+1)
+k = np.linspace(-10.*delta,10.*delta)
+rev_rate = [rev_rate_thresh(j, lam, beta, R, delta, t0, p) for j in k]
+
+f1 = plt.figure()
+plt.plot(k, rev_rate)
+plt.xlabel('k')
+plt.ylabel('revenue rate')
+
+print solve_thresh(lam, R, delta, t0, p)
+print rev_rate_thresh((1.-lam)*delta, lam, beta, R, delta, t0, p)
+
+
+plt.show()
