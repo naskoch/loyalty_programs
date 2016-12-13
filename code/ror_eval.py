83fd62257e0cba3a092f0e54ed6a6dac29f47958
Arpit Goel <argoel@stanford.edu>
2016-12-12 17:08:11 -0800

    ror plots

--- code/ror_eval.py
+++ code/ror_eval.py
@@ -1,14 +1,15 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
-def ror_A(k, R, v, b, p, beta):
-    delta = compute_delta(v, R, beta)
+def ror_A(k, R, v, b, p, beta, delta):
+    #delta = compute_delta(v, R, beta)
     lval = p*(k-R) / (b*delta**2) * (b*delta - (k-delta)*np.log(1+b*delta/(k-delta)))
     rval = (1-p)*b*(k-R)/(2*k)
     return lval + rval
 
-def ror_B(k, R, v, b, p, beta):
-    delta = compute_delta(v, R, beta)
+def ror_B(k, R, v, b, p, beta, delta):
+    #delta = compute_delta(v, R, beta)
     lval = p*(k-delta)*(1-v) / (b*delta**2) * (k*np.log(1 + b*delta/(k-delta)) - b*delta)
     rval = (1-p)*(1-b/2)*(1-v)
     return lval + rval
@@ -16,14 +17,28 @@ def ror_B(k, R, v, b, p, beta):
 def compute_delta(v, R, beta):
 	return max(0.,np.floor(np.log(v/(R*(1.-beta)))/np.log(beta)))
 
-v = 0.2 
-beta = 0.9
-alpha = 1 
-k = math.e/(alpha * (1-beta)) 
-R = alpha * k * v
+def pbpairplot(alpha=1):
+    v = 0.05 
+    beta = 0.95
+    k = math.e/(alpha * (1-beta)) 
+    R = alpha * k * v
+    delta = max(0., np.floor(-1.0/np.log(beta)))
+    p_range = [0.0001*j for j in range(1,10000)]
 
-p_range = [0.1*j for j in range(1,10)]
+    b_range = [0.0001*j for j in range(1,10000)] 
 
-b_range = [0.1*j for j in range(1,10)] 
+    x_range = []
+    y_range = []
 
-ror_gap = [[ror_A(k, R, v, b, p, beta) - ror_B(k, R, v, b, p, beta) for b in b_range] for p in p_range]
+    for b in b_range:
+        for p in p_range:
+            rA = ror_A(k, R, v, b, p, beta, delta)
+            rB = ror_B(k, R, v, b, p, beta, delta)
+            if rA > rB:
+                x_range.append(b)
+                y_range.append(p)
+
+    plt.plot(x_range, y_range)
+    plt.ylabel("p value")
+    plt.xlabel("b value")
+    plt.show()
